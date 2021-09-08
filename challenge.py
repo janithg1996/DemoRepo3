de1e43687f44e7f910fdfdbaaecc914b4ee5a4e1
janithg1996 <janithgunasekara94@gmail.com>
2021-09-08 16:14:40 +0000

    this folder includes all the demo files

---
+++ challenge.py
@@ -0,0 +1,16 @@
+print("Challenge 1:")
+
+# A message for the user
+message = "This is goint to be tricky ;)"
+Message = "Very tricky!"
+print(message) # show the message on the screen
+
+# perform mathemtical operations
+result = 2**3
+print("2**3", result)
+result = 5-3
+#print("5-3="), result)
+
+print("Challenge Complete!")
+
+print("Go slow on python and devops so you will success!")
